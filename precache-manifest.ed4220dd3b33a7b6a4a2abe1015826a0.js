f354cbd8b8c5bc8b4231a7545506705da549ec09
CB-Abhinav-S <abhinav.saxena@careerbuilder.com>
2020-09-30 00:40:14 +0530

    Updates

---
+++ precache-manifest.ed4220dd3b33a7b6a4a2abe1015826a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0eaed39f1bad9645d47d8281917faf",
+    "url": "/Factory-Bazaar/index.html"
+  },
+  {
+    "revision": "835075075589e8a41832",
+    "url": "/Factory-Bazaar/static/css/main.df24737b.chunk.css"
+  },
+  {
+    "revision": "d6377568e97da84c2621",
+    "url": "/Factory-Bazaar/static/js/2.0f3c1465.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Factory-Bazaar/static/js/2.0f3c1465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "835075075589e8a41832",
+    "url": "/Factory-Bazaar/static/js/main.38378f23.chunk.js"
+  },
+  {
+    "revision": "abb9fbfe05092af2835b",
+    "url": "/Factory-Bazaar/static/js/runtime-main.a3c900fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Factory-Bazaar/static/media/logo.5d5d9eef.svg"
+  }
+]);
